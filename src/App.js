a16d3b9ca2f62277557d42ab256bd79b85382bdc
Alan Bedoya <alanjbedoya@gmail.com>
2020-11-09 01:00:14 -0800

    Added style to the banner section.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import GlobalStyles from './components/globalStyles';
 import AboutMe from './pages/aboutMe';
 
 function App() {
   return (
     <div className='App'>
+      <GlobalStyles />
       <AboutMe />
     </div>
   );
--- src/components/bannerSection.js
+++ src/components/bannerSection.js
@@ -1,33 +1,63 @@
 import React from 'react';
 import portrait from '../img/portrait.jpg';
+import styled from 'styled-components';
 
-const aboutSection = () => {
+const bannerSection = () => {
   return (
-    <div>
-      <div className='description'>
+    <StyledBanner>
+      <StyledDescription>
         <div className='title'>
           <div className='hide'>
-            <h2>Welcome! I'm </h2>
+            <h2>Hey There!</h2>
           </div>
           <div className='hide'>
             <h2>
-              <span>Alan</span> Bedoya
+              I'm <span>Alan</span>
             </h2>
           </div>
           <div className='hide'>
-            <h2>true.</h2>
+            <h2>Bedoya</h2>
           </div>
         </div>
         <p>
           Lets colobarate together to create something beautiful and amazing!
         </p>
-        <button>Contact Me</button>
-      </div>
-      <div className='image'>
+        <button>Lets Connect!</button>
+      </StyledDescription>
+      <StyledImage>
         <img src={portrait} alt='Alan Bedoya' />
-      </div>
-    </div>
+      </StyledImage>
+    </StyledBanner>
   );
 };
 
-export default aboutSection;
+const StyledBanner = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const StyledDescription = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const StyledImage = styled.div`
+  flex: 1;
+  border-radius: 30% 70% 70% 30% / 30% 16% 84% 70%;
+
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+    border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
+  }
+`;
+export default bannerSection;
