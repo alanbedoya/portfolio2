1fa90d05035e436de6a882f1e09b3bea953f3d12
Alan Bedoya <alanjbedoya@gmail.com>
2020-11-08 22:10:14 -0800

    Added another section to the about me page.

---
+++ src/components/faqSection.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const faqSection = () => {
+  return (
+    <div>
+      <div className='faq'>
+        <h2>
+          Any Questions <span>FAQ</span>
+        </h2>
+        <div className='question'>
+          <h4>How Do I Start?</h4>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+              enim.
+            </p>
+          </div>
+        </div>
+        <div className='question'>
+          <h4>Daily Schedule</h4>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+              enim.
+            </p>
+          </div>
+        </div>
+        <div className='question'>
+          <h4>Different Payment Methods</h4>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+              enim.
+            </p>
+          </div>
+        </div>
+        <div className='question'>
+          <h4>How Do I Start?</h4>
+          <div className='answer'>
+            <p>What Products do you offer?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+              enim.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default faqSection;
--- src/pages/aboutMe.js
+++ src/pages/aboutMe.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BannerSection from '../components/bannerSection';
 import AboutSection from '../components/aboutSection';
+import FaqSection from '../components/faqSection';
 
 const aboutMe = () => {
   return (
     <>
       <BannerSection />
       <AboutSection />
+      <FaqSection />
     </>
   );
 };
